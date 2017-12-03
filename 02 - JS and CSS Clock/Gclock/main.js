5e141029ddcd104942b11afa7ab6e4af986a1055
Greta Moseley <greta.moseley@gmail.com>
2017-12-03 13:41:59 -0600

    "correct calculations and delete console.log"

--- 02 - JS and CSS Clock/Gclock/main.js
+++ 02 - JS and CSS Clock/Gclock/main.js
@@ -1,5 +1,5 @@
 'use strict';
-
+console.log("hiya!");
 const secondHand = document.querySelector('.second-hand');//find item with class of second-hand
 const minsHand = document.querySelector('.min-hand');//find item with class of min-hand
 const hourHand = document.querySelector('.hour-hand');//find item with class of hour-hand
@@ -12,16 +12,13 @@ function setDate(){
     secondHand.style.transform=`rotate(${secondsDegrees}deg)`;
     //add a style to element with class second-hand --transform rotate by however many seconds degrees you have above
     const mins = now.getMinutes();
-    const minsDegrees = ((mins/60)*360)+90;
+    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
-    const hour = now.getMinutes();
-    const hourDegrees = ((mins/12)*360)+90;
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-
-
-
-
 }
 setInterval(setDate,1000);//fire off every second
 
+setDate();
