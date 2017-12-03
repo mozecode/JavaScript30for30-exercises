ddc94ad6fc35d44772333d23bd4fbb3b6dab5995
Greta Moseley <greta.moseley@gmail.com>
2017-12-03 13:23:33 -0600

    "add minute hand functionality"

--- 02 - JS and CSS Clock/Gclock/main.js
+++ 02 - JS and CSS Clock/Gclock/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const secondHand = document.querySelector('.second-hand');//find item with class of second-hand
+const minsHand = document.querySelector('.min-hand');//find item with class of min-hand
+const hourHand = document.querySelector('.hour-hand');//find item with class of hour-hand
 
 function setDate(){
     const now = new Date();
@@ -9,6 +11,10 @@ function setDate(){
 
     secondHand.style.transform=`rotate(${secondsDegrees}deg)`;
     //add a style to element with class second-hand --transform rotate by however many seconds degrees you have above
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins/60)*360)+90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
 
 }
 setInterval(setDate,1000);//fire off every second
